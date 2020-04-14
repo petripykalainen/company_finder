cfc8e6d37c2c19b210415f1e7ef81a0e92e38cd5
petripykalainen <petri.pykalainen@bastu.net>
2020-04-14 18:53:42 +0300

    Register api in mainproject

--- api/api/settings.py
+++ api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'company.apps.CompanyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
