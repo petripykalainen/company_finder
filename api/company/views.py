26546e0a7eec1532e5eecf0f2375e6907556a2b5
petripykalainen <petri.pykalainen@bastu.net>
2020-04-15 20:58:42 +0300

    Fix datetime formatting

--- api/company/views.py
+++ api/company/views.py
@@ -3,10 +3,14 @@ from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 from dateutil.parser import parse
 from django.core import serializers
+from django.utils import timezone
+from django.utils.dateparse import parse_date
+from django.utils.dateparse import parse_datetime
 
-import datetime
 import logging
 import requests
+import datetime
+import pytz
 
 from .models import Company
 
@@ -29,20 +33,32 @@ def index(request):
         return JsonResponse({"error": result})
 
     # Check DB for company info
-    result = list(Company.objects.filter(
-        registrationDate=datetime.date(
-            date_query.year,
-            date_query.month,
-            date_query.day)).values(
-                'businessId',
-                'registrationDate',
-                'companyForm',
-                'detailsUri',
-                'name',
-            ))
+    # datetime.date(
+        # date_query.year,
+        # date_query.month,
+        # date_query.day)
 
-    # Query api for total count and then requery for all data
+    date_str = request.GET.get('date')
+    date_format = '%Y-%m-%d'
+    query_date = pytz.utc.localize(datetime.datetime.strptime(
+        date_str, date_format))
+
+    result = list(Company.objects.filter(
+        registrationDate=query_date).values(
+            'businessId',
+            'registrationDate',
+            'companyForm',
+            'detailsUri',
+            'name',
+        ))
+
+    # # Query api for total count and then requery for all data
     if len(result) == 0:
+
+        date_real = "{}-{}-{}".format(
+            '%02d' % date_query.year,
+            '%02d' % date_query.month,
+            '%02d' % date_query.day)
         date_from = "{}-{}-{}".format(
             '%02d' % date_start.year,
             '%02d' % date_start.month,
@@ -51,28 +67,34 @@ def index(request):
             '%02d' % date_end.year,
             '%02d' % date_end.month,
             '%02d' % date_end.day)
+
         url = ('http://avoindata.prh.fi/bis/v1?'
                'totalResults=true'
                '&maxResults=1000'
                '&companyRegistrationFrom={}'
                '&companyRegistrationTo={}').format(date_from, date_to)
+
         response = requests.get(url)
-        result = response.json()["results"]
+        query_result = response.json()["results"]
+
+        for company in query_result:
+            validdate = date_real == company["registrationDate"]
+            if validdate:
+                result.append(company)
 
         # Insert data to DB and result object
-        for company in result:
+        for company in query_result:
             companyid = company["businessId"]
             companyindb = Company.objects.filter(businessId=companyid)
 
             if len(companyindb) == 0:
-                logger.error("Saving to DB!")
-                logger.error(company['name'])
-                logger.error(company['registrationDate'])
                 try:
                     new_company = Company()
                     new_company.businessId = company["businessId"]
-                    new_company.registrationDate = datetime.datetime.strptime(
-                        company["registrationDate"], '%Y-%m-%d').date()
+                    new_c_date = pytz.utc.localize(
+                        datetime.datetime.strptime(
+                            company["registrationDate"], date_format))
+                    new_company.registrationDate = new_c_date
                     new_company.companyForm = company["companyForm"]
                     new_company.detailsUri = company["detailsUri"]
                     new_company.name = company["name"]
